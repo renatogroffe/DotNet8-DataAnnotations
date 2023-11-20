8577023cba11cc3352f253f79f2600636e99711d
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-19 22:18:57 -0300

    LengthAttribute
    LengthAttribute

---
+++ ConsoleAppLenghtStringsCollections/Models/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppLenghtStringsCollections.Models;
+
+public class Fornecedor
+{
+    [Required]
+    public string? RazaoSocial { get; set; }
+    
+    [Length(10, 11, ErrorMessage = "Numero do Telefone deve possuir 10 ou 11 caracteres")]
+    public string? Telefone { get; set; }
+
+    [Length(2, 4, ErrorMessage = "Devem ser definidos de 2 a 4 contatos")]
+    public string[]? Contatos { get; set; }
+
+    [Length(1, 3, ErrorMessage = "Devem ser definidos de 1 a 3 produtos")]
+    public List<string>? Produtos { get; set; }
+}
---
+++ ConsoleAppLenghtStringsCollections/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+using ConsoleAppLenghtStringsCollections.Models;
+
+Console.WriteLine("***** Testes com .NET 8 | LengthAttribute *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var fornecedores = new List<Fornecedor>();
+
+fornecedores.Add(new Fornecedor
+{
+    RazaoSocial = "Acme Produtos de Limpeza Ltda",
+    Telefone = "1134567890",
+    Contatos = new [] {"Joao Silva", "Maria Oliveira", "Joaquim Martins", "Pedro Silva" },
+    Produtos = new() { "Vassoura", "Desinfetante", "Detergente" }
+});
+
+fornecedores.Add(new Fornecedor
+{
+    RazaoSocial = "XYZ Limpeza Ltda",
+    Telefone = "11",
+    Contatos = new[] { "Joao Oliveira", "Maria Silva" },
+    Produtos = new() { }
+});
+
+fornecedores.Add(new Fornecedor
+{
+    RazaoSocial = "ABC Produtos de Limpeza Ltda",
+    Telefone = "11987654321",
+    Contatos = new[] { "Joao Almeida" },
+    Produtos = new() { "Sabao em Po", "Alcool 70", "Detergente" }
+});
+
+fornecedores.Add(new Fornecedor
+{
+    RazaoSocial = "FGH Limpeza Ltda",
+    Telefone = "119123456789",
+    Contatos = new[] { "Adao Silva", "Fabiio Oliveira", "Joao Martins", "Maria Freitas", "Pedro Martins" },
+    Produtos = new() { "Sabao em Po", "Vassoura", "Desinfetante", "Detergente" }
+});
+
+foreach (var item in fornecedores)
+{
+    Console.WriteLine();
+    Console.WriteLine(JsonSerializer.Serialize(item));
+    var validationResults = new List<ValidationResult>();
+    if (!Validator.TryValidateObject(item, new ValidationContext(item),
+        validationResults, validateAllProperties: true))
+    {
+        Console.WriteLine("Dados invalidos para essa instancia...");
+        foreach (var validationResult in validationResults)
+            Console.WriteLine($"ErrorMessage = {validationResult.ErrorMessage}");
+    }
+}
