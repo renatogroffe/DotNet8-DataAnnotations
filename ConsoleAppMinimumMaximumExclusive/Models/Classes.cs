37ee453dbdd45b7a72560407ea90d343de126e77
Renato Groffe <renato.groffe@yahoo.com.br>
2023-11-19 22:23:45 -0300

    Maximum Mininum Exclusive
    Maximum Mininum Exclusive

---
+++ ConsoleAppMinimumMaximumExclusive/Models/Classes.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConsoleAppMinimumMaximumExclusive.Models;
+
+public class AnoEraComum
+{
+    [Range(minimum: 0, maximum: int.MaxValue, MinimumIsExclusive = true)]
+    public int Valor { get; set; }
+    
+    public string Ano => $"{Valor} d.C.";
+
+    [Required]
+    public string? FatoRelevante { get; set; }
+}
+
+public class AnoAnteriorEraComum
+{
+    [Range(minimum: int.MinValue, maximum: 0, MaximumIsExclusive = true)]
+    public int Valor { get; set; }
+
+    public string Ano => $"{Valor * -1} a.C.";
+
+    [Required]
+    public string? FatoRelevante { get; set; }
+}
---
+++ ConsoleAppMinimumMaximumExclusive/Program.cs
@@ -0,0 +1,41 @@
+﻿using ConsoleAppMinimumMaximumExclusive.Models;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+using System.Text.Json;
+
+Console.WriteLine("***** Testes com .NET 8 | MinimumIsExclusive e MaximumIsExclusive *****");
+Console.WriteLine($"Versao do .NET em uso: {RuntimeInformation
+    .FrameworkDescription} - Ambiente: {Environment.MachineName} - Kernel: {Environment
+    .OSVersion.VersionString}");
+
+var anosEraComum = new AnoEraComum[]
+{
+    new() { Valor = 476, FatoRelevante = "Queda do Imperio Romano do Ocidente" },
+    new() { Valor = 1492, FatoRelevante = "Descoberta da America" },
+    new() { Valor = 0, FatoRelevante = "Ano invalido" }
+};
+foreach (var anoEraComum in anosEraComum)
+    ValidarAno<AnoEraComum>(anoEraComum);
+
+var anosEraAnteriorComum = new AnoAnteriorEraComum[]
+{
+    new() { Valor = 0, FatoRelevante = "Outro ano invalido" },
+    new() { Valor = -753, FatoRelevante = "Fundacao de Roma" },
+    new() { Valor = -2500, FatoRelevante = "Construcao da Grande Esfinge" }
+};
+foreach (var anoEraAnteriorComum in anosEraAnteriorComum)
+    ValidarAno<AnoAnteriorEraComum>(anoEraAnteriorComum);
+
+static void ValidarAno<T>(T ano)
+{
+    Console.WriteLine();
+    Console.WriteLine(JsonSerializer.Serialize(ano));
+    var validationResults = new List<ValidationResult>();
+    if (!Validator.TryValidateObject(ano!, new ValidationContext(ano!),
+        validationResults, validateAllProperties: true))
+    {
+        Console.WriteLine("Dados invalidos para essa instancia...");
+        foreach (var validationResult in validationResults)
+            Console.WriteLine($"ErrorMessage = {validationResult.ErrorMessage}");
+    }
+}
